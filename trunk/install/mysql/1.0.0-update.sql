c3825d8e11cb4891fed3801b06ec9aec8d785006
tsmr <tsmr@thetsmr.fr>
2013-01-23 16:19:02 +0000

    Drop Query Update (query is in GLPI migration)
    git-svn-id: https://forge.glpi-project.org/svn/ocsinventoryng@237 521019e7-676f-4c92-9f5a-82357c860469

--- trunk/install/mysql/1.0.0-update.sql
+++ trunk/install/mysql/1.0.0-update.sql
@@ -138,5 +138,3 @@ INSERT INTO `glpi_plugin_ocsinventoryng_registrykeys` SELECT * FROM `ocs_glpi_re
 UPDATE `glpi_displaypreferences` SET `itemtype` = 'PluginOcsinventoryngOcsServer' WHERE `itemtype` = 'OcsServer';
 
 UPDATE `glpi_crontasks` SET `itemtype` = 'PluginOcsinventoryngOcsServer' WHERE `itemtype` = 'OcsServer';
-
-UPDATE `glpi_rules` SET `sub_type` =  'RuleImportEntity' WHERE `sub_type` =  'RuleOcs';
