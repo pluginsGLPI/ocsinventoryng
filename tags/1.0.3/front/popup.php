b3846fcb9a2df79ed9c23922cb669a47efc91c5c
yllen <nini.lasson@orange.fr>
2014-09-07 16:13:59 +0000

    create tag 1.0.3
    git-svn-id: https://forge.glpi-project.org/svn/ocsinventoryng@423 521019e7-676f-4c92-9f5a-82357c860469

---
+++ tags/1.0.3/front/popup.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * @version $Id: HEADER 15930 2012-12-15 11:10:55Z tsmr $
+-------------------------------------------------------------------------
+Ocsinventoryng plugin for GLPI
+Copyright (C) 2012-2013 by the ocsinventoryng plugin Development Team.
+
+https://forge.indepnet.net/projects/ocsinventoryng
+-------------------------------------------------------------------------
+
+LICENSE
+
+This file is part of ocsinventoryng.
+
+Ocsinventoryng plugin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Ocsinventoryng plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ocsinventoryng. If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------- */
+
+include ('../../../inc/includes.php');
+
+Session::checkLoginUser();
+
+if (isset($_GET["popup"])) {
+   $_SESSION["glpipopup"]["name"] = $_GET["popup"];
+}
+
+if (isset($_SESSION["glpipopup"]["name"])) {
+   switch ($_SESSION["glpipopup"]["name"]) {
+      case "test_rule" :
+         popHeader(_x('button', 'Test'), $_SERVER['PHP_SELF']);
+         include GLPI_ROOT . "/front/rule.test.php";
+         break;
+
+      case "test_all_rules" :
+         popHeader(__('Test rules engine'), $_SERVER['PHP_SELF']);
+         include GLPI_ROOT . "/front/rulesengine.test.php";
+         break;
+
+      case "show_cache" :
+         popHeader(__('Information about cache'), $_SERVER['PHP_SELF']);
+         include GLPI_ROOT . "/front/rule.cache.php";
+         break;
+   }
+   echo "<div class='center'><br><a href='javascript:window.close()'>".__('Close')."</a>";
+   echo "</div>";
+   popFooter();
+}
+?>
---
+++ tags/1.0.3/tools/testsync.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * @version $Id$
+-------------------------------------------------------------------------
+Ocsinventoryng plugin for GLPI
+Copyright (C) 2012-2013 by the ocsinventoryng plugin Development Team.
+
+https://forge.indepnet.net/projects/ocsinventoryng
+-------------------------------------------------------------------------
+
+LICENSE
+
+This file is part of ocsinventoryng.
+
+Ocsinventoryng plugin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Ocsinventoryng plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ocsinventoryng. If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------- */
+
+// Ensure current directory when run from crontab
+chdir(dirname($_SERVER["SCRIPT_FILENAME"]));
+
+include ('../../../inc/includes.php');
+
+ini_set('display_errors',1);
+restore_error_handler();
+
+if (!isset($_SERVER['argv'][1])) {
+   die("usage testsync.php <computerid>\n");
+}
+
+$link = new PluginOcsinventoryngOcslink();
+if (!$link->getFromDBforComputer($_SERVER['argv'][1])) {
+   die("unknow computer\n");
+}
+printf("Device: %s\n", $link->getField('ocs_deviceid'));
+$timer = new Timer();
+$timer->start();
+
+$prof = new XHProf("OCS sync");
+PluginOcsinventoryngOcsServer::updateComputer($link->getID(), $link->getField('plugin_ocsinventoryng_ocsservers_id'), 1, 1);
+unset($prof);
+
+printf("Done in %s\n", $timer->getTime());
