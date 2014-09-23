c79503aa0ace2964f23ba1fefe84a6c6536299c1
tsmr <tsmr@thetsmr.fr>
2014-09-23 12:52:20 +0000

    Clean migration
    git-svn-id: https://forge.glpi-project.org/svn/ocsinventoryng@432 521019e7-676f-4c92-9f5a-82357c860469

--- trunk/hook.php
+++ trunk/hook.php
@@ -2015,7 +2015,7 @@ function plugin_ocsinventoryng_item_transfer($options=array()) {
  * @param $migration
 **/
 function plugin_ocsinventoryng_migrateComputerLocks(Migration $migration) {
-   global $DB;
+   global $DB,$CFG_GLPI;
 
    $import = array('import_printer'    => 'Printer',
                    'import_monitor'    => 'Monitor',
