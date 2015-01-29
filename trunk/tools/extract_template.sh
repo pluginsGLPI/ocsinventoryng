035e08a4602f604d9ad61a208382ef83d558a3c7
tsmr <tsmr@thetsmr.fr>
2015-01-29 15:07:09 +0000

    Update locales
    git-svn-id: https://forge.glpi-project.org/svn/ocsinventoryng@473 521019e7-676f-4c92-9f5a-82357c860469

--- trunk/tools/extract_template.sh
+++ trunk/tools/extract_template.sh
@@ -5,3 +5,6 @@
 xgettext *.php */*.php --copyright-holder='Ocsinventoryng Development Team' --package-name='GLPI - Ocsinventoryng plugin' --package-version='1.1.0' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
 	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
 	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
+
+
+
