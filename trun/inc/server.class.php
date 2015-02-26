522fa4039c87e4a75b417d767eadec360a80f4df
tsmr <tsmr@thetsmr.fr>
2015-02-26 09:16:49 +0000

    git-svn-id: https://forge.glpi-project.org/svn/ocsinventoryng@484 521019e7-676f-4c92-9f5a-82357c860469

---
+++ trun/inc/server.class.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * @version $Id: HEADER 15930 2012-12-15 11:10:55Z tsmr $
+-------------------------------------------------------------------------
+Ocsinventoryng plugin for GLPI
+Copyright (C) 2012-2013 by the ocsinventoryng plugin Development Team.
+
+https://forge.indepnet.net/projects/ocsinventoryng
+-------------------------------------------------------------------------
+
+LICENSE
+
+This file is part of ocsinventoryng.
+
+Ocsinventoryng plugin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Ocsinventoryng plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ocsinventoryng. If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------- */
+
+class PluginOcsinventoryngServer extends CommonDBTM {
+
+   
+   function getFromDBbyOcsServer($id) {
+      global $DB;
+
+      $query = "SELECT *
+                FROM `".$this->getTable()."`
+                WHERE `plugin_ocsinventoryng_ocsservers_id` = '$id'";
+
+      if ($result = $DB->query($query)) {
+         if ($DB->numrows($result) != 1) {
+            return false;
+         }
+         $this->fields = $DB->fetch_assoc($result);
+         if (is_array($this->fields) && count($this->fields)) {
+            return true;
+         }
+      }
+      return false;
+   }
+}
+?>
