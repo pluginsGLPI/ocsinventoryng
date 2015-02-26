be7f073893eb7a34ae4e378decc53641caf93457
tsmr <tsmr@thetsmr.fr>
2015-02-26 16:55:08 +0000

    Update version
    git-svn-id: https://forge.glpi-project.org/svn/ocsinventoryng@490 521019e7-676f-4c92-9f5a-82357c860469

--- branches/0.85-bugfixes/setup.php
+++ branches/0.85-bugfixes/setup.php
@@ -130,7 +130,7 @@ function plugin_init_ocsinventoryng() {
 function plugin_version_ocsinventoryng() {
 
    return array('name'           => "OCS Inventory NG",
-                'version'        => '1.1.0',
+                'version'        => '1.1.1',
                 'author'         => 'Remi Collet, Nelly Mahu-Lasson, David Durieux, Xavier Caillaud, Walid Nouh, Arthur Jaouen',
                 'license'        => 'GPLv2+',
                 'homepage'       => 'https://forge.indepnet.net/repositories/show/ocsinventoryng',
