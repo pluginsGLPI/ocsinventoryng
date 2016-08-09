d64048897fa185f14cb8c68a228cfe8c66e469e7
chrismako <chrismakoundji@yahoo.fr>
2016-08-09 09:24:38 +0200

    minor fix links for non inventoried, inventoried and identified

---
+++ front/ipdiscoverinventoriedcomputers.form.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include ('../../../inc/includes.php');
+var_dump($_POST,$_GET);
+Session::checkSeveralRightsOr(array("plugin_ocsinventoryng"       => READ,
+    "plugin_ocsinventoryng_clean" => READ));
+//Session::checkRight("plugin_ocsinventoryng", READ);
+Html::header('OCS Inventory NG', '', "tools", "pluginocsinventoryngmenu", "ipdiscmodifynetwork");
+$ip = new PluginOcsinventoryngIpDiscover();
+if (isset($_GET["ip"]) || isset($_POST["ip"])) {
+   $ipAdress = "";
+   if (isset($_GET["ip"])) {
+      $ipAdress = $_GET["ip"];
+   } else {
+      $ipAdress = $_POST["ip"];
+   }
+   $hardware = array();
+   if (isset($_GET["status"])) {
+      $hardware = $ip->getHardware($ipAdress, $_SESSION["plugin_ocsinventoryng_ocsservers_id"],$_GET["status"]);
+      $lim      = count($hardware);
+      if ($lim > $_SESSION["glpilist_limit"]) {
+         if(isset($_GET["start"])){
+         $ip->showHardware($hardware, $_SESSION["glpilist_limit"],intval($_GET["start"]), $ipAdress, $_GET["status"]);
+         } else {
+            $ip->showHardware($hardware, $_SESSION["glpilist_limit"],0, $ipAdress, $_GET["status"]);
+         }
+      } else {
+         if(isset($_GET["start"])){
+         $ip->showHardware($hardware, $lim,intval($_GET["start"]), $ipAdress, $_GET["status"]);
+         } else {
+            $ip->showHardware($hardware, $lim,0, $ipAdress, $_GET["status"]);
+         }
+      }
+   } 
+}
+html::footer();
+?>
