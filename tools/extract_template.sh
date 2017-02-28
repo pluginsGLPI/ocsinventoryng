7769fd2e5e775a566754187b0747abf14b01777c
amma35 <amandine.manceau@infotel.com>
2017-02-28 09:53:45 +0100

    update locales

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -2,9 +2,6 @@
 
 # Only strings with domain specified are extracted (use Xt args of keyword param to set number of args needed)
 
-xgettext *.php */*.php --copyright-holder='Ocsinventoryng Development Team' --package-name='GLPI - Ocsinventoryng plugin' --package-version='1.1.0' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
+xgettext *.php */*.php --copyright-holder='Ocsinventoryng Development Team' --package-name='GLPI - Ocsinventoryng plugin' --package-version='1.3.3' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
 	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
 	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
-
-
-
