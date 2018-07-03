24ddd4a2eed76a2e73cf9bc370c51a296ffee3ae
tsmr <thetsmr@gmail.com>
2018-07-03 15:18:40 +0200

    Minor fix for Mydashboard

--- inc/dashboard.class.php
+++ inc/dashboard.class.php
@@ -296,14 +296,9 @@ class PluginOcsinventoryngDashboard extends CommonGLPI {
             $title  = __("Detail of imported computers", "ocsinventoryng");
             $widget->setWidgetTitle($title);
 
-            $colors             = [
-               "#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c",
-               "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5",
-               //               "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f",
-               //               "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"
-            ];
+            $palette            = PluginMydashboardColor::getColors(2);
+            $backgroundPieColor = json_encode($palette);
             $dataPieset         = json_encode($counts);
-            $backgroundPieColor = json_encode($colors);
             $labelsPie          = json_encode($name);
 
             $graph = "<script type='text/javascript'>
