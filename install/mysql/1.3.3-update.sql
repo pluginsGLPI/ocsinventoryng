0769c3ef9a760eba992331a551dd043e6790e9c5
keha35 <kevin.hainry@infotel.com>
2018-11-06 10:29:25 +0100

    Error in SQL Update

--- install/mysql/1.3.3-update.sql
+++ install/mysql/1.3.3-update.sql
@@ -4,7 +4,7 @@ CREATE TABLE `glpi_plugin_ocsinventoryng_ruleimportentities` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 ALTER TABLE `glpi_plugin_ocsinventoryng_ocsservers` ADD `use_checkruleimportentity` TINYINT(1) NOT NULL DEFAULT '0';
-ALTER TABLE `glpi_plugin_ocsinventoryng_ocsservers` ADD `import_teamviewer` TINYINT(1) NOT NULL DEFAULT '0';"
+ALTER TABLE `glpi_plugin_ocsinventoryng_ocsservers` ADD `import_teamviewer` TINYINT(1) NOT NULL DEFAULT '0';
 
 ALTER TABLE `glpi_plugin_ocsinventoryng_winupdates` ADD `entities_id` INT(11) NOT NULL DEFAULT '0';
 
