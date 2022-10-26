ff8de0f2ba59709ec502d697e889b1ce5ef36ad1
tsmr <thetsmr@gmail.com>
2022-10-26 18:05:12 +0200

    Add missing action

--- install/mysql/2.0.0-update.sql
+++ install/mysql/2.0.0-update.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `glpi_plugin_ocsinventoryng_ocsservers` ADD `link_with_user` tinyint NOT NULL DEFAULT '1';
+UPDATE `glpi_plugin_ocsinventoryng_networkports` SET `items_devicenetworkcards_id` = '0' WHERE `items_devicenetworkcards_id` = '-1';
